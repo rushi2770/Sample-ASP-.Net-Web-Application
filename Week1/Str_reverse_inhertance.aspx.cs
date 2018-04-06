9dd2f10d5ec760b79f05dea4c584cfd22bfabd85
rushi2770 <rushikeshchikka@gmail.com>
2018-04-06 16:06:24 -0700

    dnjan

--- Week1/Str_reverse_inhertance.aspx.cs
+++ Week1/Str_reverse_inhertance.aspx.cs
@@ -5,7 +5,7 @@ namespace Week1
 {
     public partial class Str_reverse_inhertance : System.Web.UI.Page
     {
-        //added this comment 
+        //added this comMent
         protected void Page_Load(object sender, EventArgs e)
         {
 
